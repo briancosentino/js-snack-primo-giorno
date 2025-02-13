ec0b8f65cc7f79a1a14d006d77cf3ebebb7fb162
briancosentino <cosentinobrian@gmail.com>
2025-02-13 10:38:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/* L'utente inserisce due numeri in successione, con due prompt.
+il software stampa il maggiore */
+const firstPrompt = Number(prompt('inserisci il primo numero'));
+const secondPrompt = Number(prompt('inserisci il secondo numero'));
+const greaterNumber = []
+if(firstPrompt>secondPrompt){
+    greaterNumber.push(firstPrompt);
+    
+} else if( secondPrompt>firstPrompt){
+    greaterNumber.push(secondPrompt);
+    
+}
+console.log(greaterNumber);
+
+
