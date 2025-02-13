b6198188d2fcaeeff1344ce526dd0cf30ca2520e
briancosentino <cosentinobrian@gmail.com>
2025-02-13 12:12:17 +0100

    fix for loop

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -3,7 +3,7 @@ Il programma stampa la somma di tutti i numeri inseriti. */
 let numbPrompt;
 const allNumb = []
 let sum = 0;
-for (let i = 0; i < 3; i++){
+for (let i = 0; i < 10; i++){
     numbPrompt = Number(prompt('scrivi un numero '))
     allNumb.push(numbPrompt)
     sum += allNumb[i]
