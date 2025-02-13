604ce193605fe7174b9aa8ecc4fad85111d58bed
briancosentino <cosentinobrian@gmail.com>
2025-02-13 12:10:20 +0100

    add exercise 3

---
+++ jsnack3/main.js
@@ -0,0 +1,15 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+let numbPrompt;
+const allNumb = []
+let sum = 0;
+for (let i = 0; i < 3; i++){
+    numbPrompt = Number(prompt('scrivi un numero '))
+    allNumb.push(numbPrompt)
+    sum += allNumb[i]
+    
+    
+}
+console.log(sum);
+
+
